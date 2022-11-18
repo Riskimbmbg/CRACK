682e448265ec8ec45ede43cd2c772e15f4c1ea37
Rizqi Mbmbg <97333730+Riskimbmbg@users.noreply.github.com>
2022-11-18 11:15:08 +0700

    Add files via upload

---
+++ CRACK.sh
@@ -0,0 +1,635 @@
+clear
+		#Cie Ngintip
+		#Babus Ya Sc Gua
+		#Mending Lu Sholat Dari Pada Ngintip
+		#Jangan Di edit Ntar Gak Bisa Di Update!!
+
+
+#logo
+
+
+figlet Bahan Script
+	echo
+	echo
+	echo "----|Sudah Punya Bahanya?          |----"
+sleep 1
+	echo "----|ketik INSTALL yang kalo belum |----"
+sleep 1
+	echo "----|ketik YA kalo sudah           |----"
+read -p "Sudah punya? : " coy
+
+if [ $coy = INSTALL ] || [ $coy = INSTALL ]
+then
+clear
+	echo "Menginstall Bahan!"
+	sleep 1
+	echo "Loading..."
+	sleep 5
+	echo
+	echo "[✓] Sudah Terinstall"
+	sleep 4
+	bash CRACK.sh
+fi
+
+
+if [ $coy = YA ] || [ $coy = YA ]
+then
+clear
+figlet Masuk Script
+	sleep 2
+	echo
+	echo "Terimakasih"
+	echo "Silahkan Menikmati Script Saya"
+	sleep 4
+fi
+
+clear
+sleep 2
+	echo
+	echo "__________________________________"
+	echo "|                                |"
+        echo "|    Author   : MRIZP            |"
+        echo "|    Youtube  : Riskimbmbg       |"
+        echo "|    WhatsApp : 081387603153     |"
+        echo "__________________________________"
+        echo "-------Crack Facebook 2022-------"
+	echo
+
+#password
+        read -p "[‣] Nama Kamu   : " username
+        read -p "[‣] Sandi Login : " pass
+
+echo $pass $username
+sleep 3
+echo
+if [ $pass = "CUKUP-TAU-YTTA" ]
+        then
+	echo [‣]"Mengecek Sandi Yang Di Masukan"
+	sleep 3
+	echo "____________________________________"
+	echo
+	echo [‣]"Mohon Tunggu 5 Detik"
+	echo "____________________________________"
+	sleep 5
+	echo [✓]"Sandi Benar"
+        sleep 3
+clear
+        else
+	echo [‣]"Mengecek Sandi Yang Di Masukan"
+	sleep 2
+	echo "____________________________________"
+	echo
+	echo [‣]"Mohon Tunggu 5 Detik"
+	echo "____________________________________"
+	sleep 5
+	echo
+        echo [✘]"Sandi Salah"
+	sleep 3
+	echo
+        echo [ↆ]"Loading Ulang..."
+        sleep 5
+        bash CrackFb.sh
+        sleep 4
+
+fi
+sleep 3
+
+#logo
+
+
+	echo
+	echo "__________________________________"
+	echo "|                                |"
+	echo "|    Author   : Rizqi            |"
+	echo "|    Youtube  : Riskimbmbg       |"
+	echo "|    WhatsApp : 081387603153     |"
+	echo "__________________________________"
+	echo
+	sleep 4
+	echo
+ 	echo "⸻⸻⸻⸻⸻⸻⸻⸻⸻ Menu Script ⸻⸻⸻⸻⸻⸻⸻⸻⸻"
+	sleep 5
+        echo
+        echo [1]‣"Crack Fb Masal"
+	sleep 1
+	echo [2]‣"Crack Fb Target"
+        sleep 1
+	echo [3]‣"Laporkan Script Jika Eror"
+	sleep 1
+        echo [4]‣"Exit Script"
+	sleep 1
+        echo
+        read -p "Pilihanmu ‣ : " bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+
+echo
+echo "__________________________________"
+echo "|                                |"
+echo "|    Author   : Rizqi            |"
+echo "|    Youtube  : Riskimbmbg       |"
+echo "|    WhatsApp : 081387603153     |"
+echo "__________________________________"
+echo
+sleep 4
+echo "Crack Masal v1"
+sleep 2
+echo "Silahkan Aktifkan mode pesawat 5 detik"
+sleep 7
+echo "Admin tidak bertanggung jika terkena checkpoin!"
+sleep 5
+echo
+echo "Ketik Y Untuk Melanjutkan!"
+echo "Ketik N Untuk Stop"
+read -p "Ingin Melanjutkan Crack? : " bro
+fi
+
+if [ $bro = Y ] || [ $bro = Y ]
+then
+clear
+
+echo "Mencari Data Akun Random.."
+echo "Mencoba Login Ke Akun.."
+echo
+echo
+echo
+echo "_______________________________________________________"
+sleep 7
+echo "Login To https://www.facebook.com/profile.php?id=100087942880834"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/profile.php?id=100081928159501"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/profile.php?id=100069287353359"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/xzfbryn21"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/irfan.ivanofic"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/bagas.aryasaputra.37"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/indah.aritonang.5055"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/allo.wnda.75"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/profile.php?id=100045481853588"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/profile.php?id=100072154268338"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/il.ma.3998"
+sleep 5
+echo "[✓] Succes Login"
+sleep 5
+echo "Login To https://www.facebook.com/soja.kumala.5"
+sleep 5
+echo "[✓] Succes Login"
+echo
+echo
+echo
+sleep 7
+echo "Mencoba Mencari Password..."
+echo
+echo "|⸻⸻⸻ Masuk Dengan Id? [ Oke ] ⸻⸻⸻"
+echo "|⸻⸻⸻ Tidak Dengan Id? [  X  ] ⸻⸻⸻"
+	read -p "⸻⸻⸻>>> Pilih Oke/X <<<⸻⸻⸻ : " cok
+fi
+
+if [ $cok = Oke ] || [ $cok = Oke ]
+then
+clear
+echo
+echo
+echo "__________________________________"
+echo "|                                |"
+echo "|    Author   : Rizqi            |"
+echo "|    Youtube  : Riskimbmbg       |"
+echo "|    WhatsApp : 081387603153     |"
+echo "__________________________________"
+echo
+echo "Crack Fb Berlangsung ‣"
+sleep 3
+echo
+echo "Loading..."
+sleep 7
+echo "|"
+echo "|"
+echo "|"
+echo "|"
+echo "⸻‣Id User   : 100087942880834"
+echo "⸻‣User Name : Nirfanda"
+echo "⸻‣Password  : NandaAyuNingsih"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : 100085424014104"
+echo "⸻‣User Name : Pak Eko"
+echo "⸻‣Password  : Ekonuri501"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : 100076801296938"
+echo "⸻‣User Name : Wulan Lan"
+echo "⸻‣Password  : Kaila359"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : xzfbryn21"
+echo "⸻‣User Name : Febriani"
+echo "⸻‣Password  : Febridian"
+sleep 3
+echo "Status      : Rentan Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : irfan.ivanofic"
+echo "⸻‣User Name : IranApe"
+echo "⸻‣Password  : irfan bagas isnandar"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : bagas.aryasaputra.37"
+echo "⸻‣User Name : Bagas Arya Saputra"
+echo "⸻‣Password  : BagasGampeng37"
+sleep 3
+echo "Status      : Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : indah.aritonang.5055"
+echo "⸻‣User Name : Indah Malasari Aritonang"
+echo "⸻‣Password  : IndahMalasari5055"
+sleep 3
+echo "Status      : Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : allo.wnda.75"
+echo "⸻‣User Name : Allo Wnda"
+echo "⸻‣Password  : Winda Safitri75"
+sleep 3
+echo "Status      : Rentan Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : 100045481853588"
+echo "⸻‣User Name : Ilham Fauzi Kclk"
+echo "⸻‣Password  : Ilham Kclk123488"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : 100072154268338"
+echo "⸻‣User Name : Andrea muhammad irawan"
+echo "⸻‣Password  : Andre Gace338"
+sleep 3
+echo "Status      : Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : il.ma.3998"
+echo "⸻‣User Name : Ilma"
+echo "⸻‣Password  : Ilma Wati Kurnia Wati3998"
+sleep 3
+echo "Status      : Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣Id User   : soja.kumala.5"
+echo "⸻‣User Name : Soja Kumala"
+echo "⸻‣Password  : Soja kumala555"
+sleep 3
+echo "Status      : Rentan Chek Poin [•]"
+sleep 3
+bash CRACK.sh
+sleep 3
+fi
+
+if [ $cok = X ] || [ $cok = X ]
+then
+clear
+echo
+echo
+echo "__________________________________"
+echo "|                                |"
+echo "|    Author   : Rizqi            |"
+echo "|    Youtube  : Riskimbmbg       |"
+echo "|    WhatsApp : 081387603153     |"
+echo "__________________________________"
+echo
+echo "Crack Fb Berlangsung ‣"
+sleep 3
+echo
+echo "Loading..."
+sleep 7
+echo "|"
+echo "|"
+echo "|"
+echo "|"
+echo "⸻‣User Name : Nirfanda"
+echo "⸻‣Password  : NandaAyuNingsih"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣User Name : Pak Eko"
+echo "⸻‣Password  : Ekonuri789"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣User Name : Wulan Lan"
+echo "⸻‣Password  : Kaila999"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣User Name : Febriani"
+echo "⸻‣Password  : Febridian"
+sleep 3
+echo "Status      : Rentan Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣User Name : irfan"
+echo "⸻‣Password  : irfan bagas isnandar"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣User Name : Bagas Arya Saputra"
+echo "⸻‣Password  : BagasGampeng"
+sleep 3
+echo "Status      : Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣User Name : Indah Malasari Aritonang"
+echo "⸻‣Password  : IndahMalasari917"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"                                                                                                                                                
+echo "|"
+echo "⸻‣User Name : Allo Wnda"
+echo "⸻‣Password  : Winda Safitri15"
+sleep 3
+echo "Status      : Rentan Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣User Name : Ilham Fauzi Kclk"
+echo "⸻‣Password  : Ilham Kclk123"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"                                                                                                                                                
+echo "⸻‣User Name : Andrea muhammad irawan"
+echo "⸻‣Password  : Andre Gace86"
+sleep 3
+echo "Status      : Chek Poin [•]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣User Name : Ilma"
+echo "⸻‣Password  : Ilma Wati Kurnia Wati"
+sleep 3
+echo "Status      : Aktif [✓]"
+sleep 3
+echo "|"
+echo "|"
+echo "⸻‣User Name : Soja Kumala"
+echo "⸻‣Password  : Soja Ahya Sari677"
+sleep 2
+echo "Status      : Rentan Chek Poin [•]"
+sleep 3
+bash CRACK.sh
+sleep 3
+fi
+
+if [ $bro = N ] || [ $bro = N ]
+then
+
+figlet Thank You
+
+sleep 2
+echo
+echo "Bye Terimakasih Sudah Mampir"
+echo "Sampai Jumpa Lain Waktu"
+echo
+sleep 5
+exit
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+
+echo
+echo "__________________________________"
+echo "|                                |"
+echo "|    Author   : Rizqi            |"
+echo "|    Youtube  : Riskimbmbg       |"
+echo "|    WhatsApp : 081387603153     |"
+echo "__________________________________"
+echo
+sleep 4
+echo "Crack Target 2022"
+sleep 2
+echo "Perhatikan Langkah Langkahnya Biar Tidak Error"
+sleep 7
+echo "Apabila Gagal Login Bisa Mulai Ulang Sc Dan Pilih Report Bug"
+sleep 5
+echo
+echo "[•] Login"
+echo "------Silahkan Ketik [ Masuk ]"
+read -p "••••>>      : " bro
+fi
+
+
+if [ $bro = Masuk ] || [ $bro = Masuk ]
+then
+clear
+echo
+echo "__________________________________"
+echo "|                                |"
+echo "|    Author   : Rizqi            |"
+echo "|    Youtube  : Riskimbmbg       |"
+echo "|    WhatsApp : 081387603153     |"
+echo "__________________________________"
+echo
+echo "Crack Fb Target 2022 ‣"
+echo "Loading..."
+echo "Jangan Menggunakan Spasi!!"
+sleep 7
+read -p "User Name Target : " woy
+echo
+fi
+
+if [ $woy = Nirfanda ] || [ $woy = Nirfanda ]
+then
+	echo "Mencari User Name..."
+	sleep 4
+	echo "[✓] User Name Di Temukan"
+	read -p "Id Akun  : " woy
+fi
+
+
+if [ $woy = 100087942880834 ] || [ $woy = 100087942880834 ]
+then
+	sleep 5
+	echo "User Name Dan Password Di Temukan"
+sleep 5
+echo
+echo
+echo "User Name Target : Nirfanda"
+sleep 3
+echo "Password Target  : NandaAyuNingsih"
+sleep 4
+echo "Status Akun      : Aktif [✓]"
+echo
+echo
+echo
+sleep 3
+echo
+echo "Mendeteksi Chek Poin"
+sleep 4
+pkg install cmatrix
+cmatrix
+sleep 5
+fi
+
+
+if [ $woy = PakEko ] || [ $woy = PakEko ]
+then
+        echo "Mencari User Name..."
+        sleep 4
+        echo "[✓] User Name Di Temukan"
+        read -p "Id Akun  : " woy
+fi
+
+
+if [ $woy = 100085424014104 ] || [ $woy = 100085424014104  ]
+then
+        sleep 5
+        echo "User Name Dan Password Di Temukan"
+sleep 5
+echo
+echo
+echo "User Name Target : Pak Eko"
+sleep 3
+echo "Password Target  : Ekonuri789"
+sleep 4
+echo "Status Akun      : Aktif [✓]"
+echo
+echo
+echo
+sleep 3
+echo
+echo "Mendeteksi Chek Poin"
+sleep 4
+pkg install cmatrix
+cmatrix
+sleep 5
+fi
+
+
+if [ $woy = WulanLan ] || [ $woy = WulanLan ]
+then
+        echo "Mencari User Name..."
+        sleep 4
+        echo "[✓] User Name Di Temukan"
+        read -p "Id Akun  : " woy
+fi
+
+
+if [ $woy = 100076801296938 ] || [ $woy = 100076801296938  ]
+then
+        sleep 5
+        echo "User Name Dan Password Di Temukan"
+sleep 5
+echo
+echo
+echo "User Name Target : Wulan Lan"
+sleep 3
+echo "Password Target  : Kaila999"
+sleep 4
+echo "Status Akun      : Aktif [✓]"
+echo
+echo
+echo
+sleep 3
+echo
+echo "Mendeteksi Chek Poin"
+sleep 4
+pkg install cmatrix
+cmatrix
+sleep 5
+fi
+
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+
+figlet SCRIPT EROR
+sleep 5
+echo
+echo "Laporkan Ke Admin"
+echo
+echo "Chat WhatsApp Admin wa.me/6281387603153"
+echo "Salin Nomer Admin Lalu Tempel Ke Nomer baru"
+echo "Jangan Lupa Sv Juga Ya"
+fi
+
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+
+figlet Keluar
+sleep 2
+echo
+echo "TERIMAKASIH SUDAH MENGGUNAKAN SCRIPT SAYA"
+echo "MOHON MAAF JIKA BANYAK SALAH"
+echo
+sleep 3
+echo "SAMPAI JUMPA LAIN WAKTU"
+exit
+fi
